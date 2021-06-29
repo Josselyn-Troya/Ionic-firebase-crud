7e81a2c0719386c46ac5f4663b6253c551c04d5f
Stefani Troya <josselyn.troya@epn.edu.ec>
2021-06-29 00:05:14 -0500

    firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
 
   firebaseConfig: {
     apiKey: "AIzaSyBmO5lIHVHctSDAcgAMT7uik1U9Aph2COI",
